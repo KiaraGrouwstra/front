0fd786ac249c851464fae213220492fbb435cb39
tycho01 <tychogrouwstra@gmail.com>
2016-06-24 00:13:47 +0800

    trust html for display in iframes

--- app/comps/lib/pipes.ts
+++ app/comps/lib/pipes.ts
@@ -1,6 +1,7 @@
 import { Pipe, PipeTransform, Injectable } from '@angular/core';
 let marked = require('marked');
 import { parseScalar } from '../slim/output/output';
+import { arr2obj } from './js';
 
 export function genPipe(fn: (any) => string, opts: PipeMetadata): PipeTransform {
   let pipe = class implements PipeTransform {
@@ -15,3 +16,13 @@ export function genPipe(fn: (any) => string, opts: PipeMetadata): PipeTransform 
 
 export let MarkedPipe = genPipe(marked, { name: 'marked' });
 export let ScalarPipe = genPipe(parseScalar, { name: 'scalar' });
+
+const trust = arr2obj(['Html', 'Style', 'Script', 'Url', 'ResourceUrl'], (type: string) => {
+  let method = 'bypassSecurityTrust' + type;
+  return (str: string, sanitizer: DomSanitizationService) => sanitizer[method](str);
+});
+export let trustHtml = genPipe(trust.Html, { name: 'trustHtml' });
+export let trustStyle = genPipe(trust.Style, { name: 'trustStyle' });
+export let trustScript = genPipe(trust.Script, { name: 'trustScript' });
+export let trustUrl = genPipe(trust.Url, { name: 'trustUrl' });
+export let trustResourceUrl = genPipe(trust.ResourceUrl, { name: 'trustResourceUrl' });
--- app/comps/slim/output/iframe/iframe.ts
+++ app/comps/slim/output/iframe/iframe.ts
@@ -1,4 +1,6 @@
 import { Component, Input, ChangeDetectionStrategy, ViewEncapsulation } from '@angular/core';
+import { DomSanitizationService } from '@angular/platform-browser';
+import { trustHtml } from '../../../lib/pipes';
 
 // this component just adds over <iframe> by adding ng2's style encapsulation
 // pimp this up so as to give the iframe in one tab, the html in another?
@@ -7,13 +9,15 @@ type Val = string;
 
 @Component({
   selector: 'myiframe',
-  template: `<iframe [srcdoc]='val'></iframe>`,
+  template: `<iframe [srcdoc]='val | trustHtml:sanitizer'></iframe>`,
   changeDetection: ChangeDetectionStrategy.OnPush,
   encapsulation: ViewEncapsulation.Native,  // Emulated
   styles: [
-  require('!raw!less!./iframe.less'),
+    require('!raw!less!./iframe.less'),
   ],
+  pipes: [trustHtml],
 })
 export class IframeComp {
   @Input() val: Val;
+  constructor(public sanitizer: DomSanitizationService) {}
 }
